62260535b76f3d7a8f7d3fcd6dfb94d17a8aff51
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-03 23:19:37 -0300

    feat: add stripe call on home page to retrieve the price

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss';
 
-export default function SubscribeButton() {
+type SubscribeButtonProps = {
+  priceId: string;
+};
+
+export default function SubscribeButton({ priceId }: SubscribeButtonProps) {
   return (
     <button type='button' className={styles.subscribeButton}>
       Subscribe now
