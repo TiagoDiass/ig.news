c77e49194455e3089ae01e3ebaa3b68192d5832b
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-04 21:36:40 -0300

    feat: add verification if user exists before inserting his data into the db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -15,10 +15,16 @@ export default NextAuth({
   callbacks: {
     async signIn(user, account, profile) {
       try {
-        await fauna.query(q.Create(q.Collection('users'), { data: { email: user.email } }));
+        await fauna.query(
+          q.If(
+            q.Not(q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))),
+            q.Create(q.Collection('users'), { data: { email: user.email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
+          )
+        );
 
         return true;
-      } catch {
+      } catch (err) {
         return false;
       }
     },
