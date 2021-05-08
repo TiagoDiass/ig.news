eb61ef86793240d058c3b70ce52d39735eea0dce
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-08 13:23:18 -0300

    refactor: connect Header to posts page and home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import SignInButton from 'components/SignInButton';
 import styles from './styles.module.scss';
 
@@ -5,13 +6,20 @@ export default function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src='/images/logo.svg' alt='ig.news' />
+        <Link href='/'>
+          <img src='/images/logo.svg' alt='ig.news' />
+        </Link>
 
         <nav>
-          <a href='#' className={styles.active}>
-            Home
-          </a>
-          <a href='#'>Posts</a>
+          <Link href='/'>
+            <a href='/' className={styles.active}>
+              Home
+            </a>
+          </Link>
+
+          <Link href='/posts'>
+            <a href='/posts'>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
