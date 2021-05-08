da31ed607d8bd39ca536b7141eca2486cd69113d
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-08 14:24:45 -0300

    feat: add posts list page completely, fetching data from prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,24 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+import { getPrismicClient } from 'services/prismic';
 import styles from './posts.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+type PostProps = {
+  posts: Post[];
+};
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -10,31 +27,45 @@ export default function Posts() {
 
       <main className={styles.postsContainer}>
         <ul className={styles.postsList}>
-          <li>
-            <a href='#' className={styles.postLink}>
-              <time>12 de março de 2021</time>
-              <strong>Creating a Monorepo with Yarn workspaces</strong>
-              <p>In this guide, you will learn how to create a Monorepo with Yarn workspaces.</p>
-            </a>
-          </li>
-
-          <li>
-            <a href='#' className={styles.postLink}>
-              <time>12 de março de 2021</time>
-              <strong>Creating a Monorepo with Yarn workspaces</strong>
-              <p>In this guide, you will learn how to create a Monorepo with Yarn workspaces.</p>
-            </a>
-          </li>
-
-          <li>
-            <a href='#' className={styles.postLink}>
-              <time>12 de março de 2021</time>
-              <strong>Creating a Monorepo with Yarn workspaces</strong>
-              <p>In this guide, you will learn how to create a Monorepo with Yarn workspaces.</p>
-            </a>
-          </li>
+          {posts.map(post => (
+            <li key={post.slug}>
+              <Link href={`/posts/${post.slug}`}>
+                <a className={styles.postLink}>
+                  <time>{post.updatedAt}</time>
+                  <strong>{post.title}</strong>
+                  <p>{post.excerpt}</p>
+                </a>
+              </Link>
+            </li>
+          ))}
         </ul>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([Prismic.Predicates.at('document.type', 'post')], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  });
+
+  const posts = response.results.map(post => ({
+    slug: post.uid,
+    title: RichText.asText(post.data.title),
+    excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+    updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    }),
+  }));
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
