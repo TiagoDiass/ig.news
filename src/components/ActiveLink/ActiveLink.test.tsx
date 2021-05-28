805e552c04eb5c2132984de5aa84fb20c696af64
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-27 22:07:31 -0300

    test: add ActiveLink tests

--- src/components/ActiveLink/ActiveLink.test.tsx
+++ src/components/ActiveLink/ActiveLink.test.tsx
@@ -1,12 +1,40 @@
 import ActiveLink from '.';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+
+jest.mock('next/router', () => {
+  return {
+    useRouter: () => ({ asPath: '/' }),
+  };
+});
 
 describe('ActiveLink component', () => {
   it('should render correctly', () => {
-    const { debug } = render(
+    render(
       <ActiveLink href='/' activeClassName='active'>
         <a>Home</a>
       </ActiveLink>
     );
+
+    expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
+  });
+
+  it('should have the active class', () => {
+    render(
+      <ActiveLink href='/' activeClassName='active'>
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(screen.getByRole('link', { name: /home/i })).toHaveClass('active');
+  });
+
+  it('should not have the active class', () => {
+    render(
+      <ActiveLink href='/test' activeClassName='active'>
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(screen.getByRole('link', { name: /home/i })).not.toHaveClass('active');
   });
 });
