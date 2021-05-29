cbafc31c2e33f457381472a82cecd845d2075d81
Tiago Dias <tiago.costadiasss@gmail.com>
2021-05-29 17:04:57 -0300

    refactor: change mocks implementations

--- src/components/SignInButton/SignInButton.test.tsx
+++ src/components/SignInButton/SignInButton.test.tsx
@@ -1,24 +1,20 @@
 import SignInButton from '.';
 import { screen, render } from '@testing-library/react';
+import nextAuthClient from 'next-auth/client';
 
-jest.mock('next-auth/client', () => {
-  return {
-    useSession: jest
-      .fn()
-      .mockImplementationOnce(() => [null, false])
-      .mockImplementationOnce(() => [{ user: { name: 'Tiago Dias' } }, true]),
-  };
-});
+const useSessionMock = jest.spyOn(nextAuthClient, 'useSession');
 
 describe('SignInButton component', () => {
   it('should render correctly when user is not logged in', () => {
+    useSessionMock.mockReturnValue([null, false]);
     render(<SignInButton />);
     expect(screen.getByRole('button', { name: /login com github/i })).toBeInTheDocument();
   });
 
   it('should render correctly when user is logged in', () => {
+    useSessionMock.mockReturnValue([{ user: { name: 'John Doe' } }, true]);
     render(<SignInButton />);
     expect(screen.queryByRole('button', { name: /login com github/i })).not.toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /tiago dias/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /john doe/i })).toBeInTheDocument();
   });
 });
