62f286b05a3538df271bb672cca7ca6d1b6771be
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-29 23:41:57 -0300

    test: ensure SubscribeButton calls signIn() when user is not logged in

--- src/components/SubscribeButton/SubscribeButton.test.tsx
+++ src/components/SubscribeButton/SubscribeButton.test.tsx
@@ -1,16 +1,28 @@
-import { render, screen } from '@testing-library/react';
 import SubscribeButton from '.';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import nextAuthClient from 'next-auth/client';
 
-jest.mock('next-auth/client', () => {
-  return {
-    useSession: () => [null, false],
-  };
-});
+// mocks
+const useSessionMock = jest.spyOn(nextAuthClient, 'useSession');
+jest.spyOn(nextAuthClient, 'signIn').mockResolvedValue({ ok: true });
+
+const getSubscribeButton = () => screen.getByRole('button', { name: /se inscreva já/i });
 
 describe('SubscribeButton component', () => {
   it('should render correctly', () => {
+    useSessionMock.mockImplementation(() => [null, false]);
+    render(<SubscribeButton />);
+    expect(getSubscribeButton()).toBeInTheDocument();
+  });
+
+  it('should should sign in when user is not logged in', () => {
+    useSessionMock.mockImplementation(() => [null, false]); // user is not logged in
     render(<SubscribeButton />);
 
-    expect(screen.getByRole('button', { name: /se inscreva já/i })).toBeInTheDocument();
+    const button = getSubscribeButton();
+    userEvent.click(button);
+
+    expect(nextAuthClient.signIn).toHaveBeenCalledWith('github');
   });
 });
